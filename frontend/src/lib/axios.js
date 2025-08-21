44f4c5c79d6802f7870b8ec0522ae90543dd7970
Harsh <hp3032004@gmail.com>
2025-08-21 12:55:51 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : '/api'
+const BASE_URL = import.meta.env.MODE === 'development' ? 'https://sketch-battle-backend.vercel.app/api' : '/api'
 
 export const axiosInstance = axios.create({
     baseURL:BASE_URL,
